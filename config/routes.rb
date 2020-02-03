53e5022337c1fa6ef23d42cd75af6bb518734c81
cache-cache-la <rote.blumen.21991@gmail.com>
2020-02-03 19:22:17 +0900

    Setting
    フロント実装前の準備を行いました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'entries/index'
+  root "entries#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
