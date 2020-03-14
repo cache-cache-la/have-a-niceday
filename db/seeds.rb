9dcc9efee3c5af6713ebfa4eeb232ff75ed8befa
cache-cache-la <rote.blumen.21991@gmail.com>
2020-03-14 18:50:27 +0900

    color
    カラーコードの変更を行いました。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+
+Status.create(condition: '新品、未使用')
+Status.create(condition: '未使用に近い')
+Status.create(condition: '目立った傷や汚れなし')
+Status.create(condition: 'やや傷や汚れあり')
+Status.create(condition: '傷や汚れあり')
+Status.create(condition: '全体的に状態が悪い')
