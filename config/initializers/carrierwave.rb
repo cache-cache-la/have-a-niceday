bc11ebea77e038c42beb0376a1724823ffc14b22
cache-cache-la <rote.blumen.21991@gmail.com>
2020-03-25 14:48:00 +0900

    調整

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production? # 本番環境ならAWSを使用
+  if Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -11,7 +11,7 @@ CarrierWave.configure do |config|
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1'
-    }
+      }
 
     config.fog_directory  = 'haveaniceday2020'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/haveaniceday2020'
